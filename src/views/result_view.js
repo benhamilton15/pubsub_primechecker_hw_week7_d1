4c16499f442f3eeea436c9d66e3fb6fce685c052
Ben Hamilton <ben.hamilton@me.com>
2019-05-13 17:21:59 +0100

    finished mvp

---
+++ src/views/result_view.js
@@ -0,0 +1,26 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const ResultView = function () {
+
+};
+
+ResultView.prototype.bindEvents = function () {
+  PubSub.subscribe('PrimeChecker:result-calculated', (event) => {
+    const numberIsPrime = event.detail;
+    this.displayResult(numberIsPrime);
+  });
+};
+
+ResultView.prototype.displayResult = function (result) {
+  const resultElement = document.querySelector('#result');
+  if (result === true) {
+    resultElement.textContent = `Yes! It's a prime number`;
+  }
+  else {
+    resultElement.textContent = `No! It's not a prime number`;
+
+  }
+  
+};
+
+module.exports = ResultView;
